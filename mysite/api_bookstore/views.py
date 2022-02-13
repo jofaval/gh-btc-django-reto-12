7db4bc369906f340a48c08c0f0929493ebbdff23
pepe_fabra_valverde <jofaval@outlook.com>
2022-02-13 17:04:56 +0100

    Add the restricted view

--- mysite/api_bookstore/views.py
+++ mysite/api_bookstore/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from .serializers import AuthorSerializer, BookSerializer
 from .models import Author, Book
 
@@ -14,3 +19,9 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('title')
     serializer_class = BookSerializer
 
+@api_view(["GET"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def welcome(request):
+    content = {"message": "Welcome to the BookStore!"}
+    return JsonResponse(content)
